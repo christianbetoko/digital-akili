48271b0b31879ebe643d7f277edd4fac870db0f4
betoko <christianbetoko@gmail.com>
2026-05-09 11:43:57 +0100

    prime7

--- resources/views/livewire/components/footer.blade.php
+++ resources/views/livewire/components/footer.blade.php
@@ -47,7 +47,7 @@
 
     <div class="container copyright text-center mt-4">
       <p>© <span>Copyright</span> <strong class="px-1 sitename">DIGITAL AKILI SARL</strong> <span>Tous droits réservés.</span></p>
-      <div class="credits">
+      <div class="credits" hidden>
       
         Designed by <a href="https://christianbetoko.dev/">C.B</a>
       </div>
